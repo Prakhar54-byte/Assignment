b044433fd1ae1b80a72c04142412a94f98e0dc6a
Prakhar Chauhan <prakharc295@gmail.com>
2024-08-22 01:17:43 +0530

    sdg

--- 03_currency_convertor/src/components/InputBox.jsx
+++ 03_currency_convertor/src/components/InputBox.jsx
@@ -3,15 +3,14 @@ import { useId } from 'react'
 
 function InputBox({
     label,
-    currency,
+    amount,
     onAmountChange,
     onCurrencyChange,
-    setCurrencyType = 'usd',
-    swapCurrency,
-    className = '',
-    currencyOptions = [],
+    currencyOption = [],
+    selectCurrency ='usd',
     amountDisable = false,
     currencyDisaabled = false,
+    className = '',
 }
 
 ) {
@@ -25,7 +24,7 @@ function InputBox({
                 <input className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount" id={id}
-                    value={currency}
+                    value={amount}
                     disabled={amountDisable}
                     onChange={(e) => onAmountChange && onAmountChange(e.target.value)}
                 />
@@ -33,11 +32,11 @@ function InputBox({
             <div className="w-1/2 flex flex-wrap justify-end text-right">
             <p className="text-black/40 mb-2 w-full">Currency Type</p>
             <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-            value={setCurrencyType}
+            value={selectCurrency}
             disabled={currencyDisaabled}
             onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-            >{currencyOptions.map((currency)=>{
-                return <option key={currency} value={currency}>{currency}</option>
+            >{currencyOption.map((currency)=>{
+                return <option key={amount} value={amount}>{amount}</option>
             })}</select></div>
 
         </div>
--- 03_currency_convertor/src/hooks/useCurrencyInfo.js
+++ 03_currency_convertor/src/hooks/useCurrencyInfo.js
@@ -4,7 +4,8 @@ import { useState,useEffect } from "react";
 function useCurrencyInfo(currency) {
     const [data, setData] = useState({});
     useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.6.11/v1/currencies/${currency}.json`).then((res)=>res.json()).
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=>res.json()).
         then((res) => setData(res[currency]))
         console.log(data);
     },[currency])
